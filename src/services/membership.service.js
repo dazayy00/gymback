74e7d9b4a636b57de302c8ab4c90a3cfcb6b4d8a
dazayy00 <zs21020562@estudiantes.uv.mx>
2026-04-17 19:38:38 -0600

    membership update

--- src/services/membership.service.js
+++ src/services/membership.service.js
@@ -3,35 +3,75 @@ import prisma from "../config/prisma.js";
 export const createMembershipService = async (data) => {
     const { userId, type } = data;
 
-    const startDate = new Date();
-    let endDate = new Date();
+    const now = new Date();
 
-    switch (type){
-        case "DAILY":
-            endDate.setDate(startDate.getDate() + 1);
-            break;
-        case "WEEKLY":
-            endDate.setDate(startDate.getDate() + 7);
-            break;
-        case "MONTHLY":
-            endDate.setMonth(startDate.getMonth() + 1);
-            break;
-        case "YEARLY":
-            endDate.setFullYear(startDate.getFullYear() + 1);
-            break;
-        default:
-            throw new Error("Tipo de membresia invalida");
+    const activeMembership = await prisma.membership.findFirst({
+        where: {
+            userId,
+            status: "ACTIVE",
+            endDate: {
+                gt: now,
+            },
+        },
+    });
+
+    const calculateEndDate = (start, type) => {
+        const end = new Date(start);
+
+        switch (type){
+            case "DAILY":
+                end.setDate(end.getDate() + 1);
+                break;
+            case "WEEKLY":
+                end.setDate(end.getDate() + 7);
+                break;
+            case "MONTHLY":
+                end.setMonth(end.getMonth() + 1);
+                break;
+            case "YEARLY":
+                end.setFullYear(end.getFullYear() + 1);
+                break;
+            default:
+                throw new Error("tipo invalido");
+        }
+
+        return end;
+    };
+
+    if (type === "DAILY") {
+        const endDate = calculateEndDate(now, type);
+
+        return await prisma.membership.create({
+            data: {
+                userId,
+                type,
+                startDate: now,
+                endDate,
+                status: "ACTIVE",
+            },
+        });
     }
 
-    const membership = await prisma.membership.create({
+    if(activeMembership) {
+        const newEndDate = calculateEndDate(activeMembership.endDate, type);
+
+        return await prisma.membership.update({
+            where: { id: activeMembership.id},
+            data: {
+                endDate: newEndDate,
+            },
+        });
+    }
+
+    const endDate = calculateEndDate(now, type);
+
+    return await prisma.membership.create({
         data: {
             userId,
             type,
-            startDate,
+            startDate: now,
             endDate,
             status: "ACTIVE",
         },
     });
-
-    return membership;
 };
